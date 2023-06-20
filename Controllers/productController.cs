98a758755633688da51ee565e94ef88d34b06ae4
MiguelPeralta123 <maikperalta123@gmail.com>
2023-06-19 17:49:14 -0700

    First commit. CRUD and IIS configuration working

---
+++ Controllers/productController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineShop.Data;
+using OnlineShop.Models;
+
+namespace OnlineShop.Controllers
+{
+    [ApiController]
+    [Route("api/products")]
+    public class productController: ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<productModel>>> getProducts()
+        {
+            var function = new ProductData();
+            var list = await function.listProducts();
+            return list;
+        }
+
+        [HttpPost]
+        public async Task postProduct([FromBody] productModel parameters)   
+        {
+            var function = new ProductData();
+            await function.insertProduct(parameters);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> putProduct(int id, [FromBody] productModel parameters)
+        {
+            var function = new ProductData();
+            parameters.id = id;
+            await function.updateProduct(parameters);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> deleteProduct(int id)
+        {
+            var function = new ProductData();
+            var parameters = new productModel();
+            parameters.id = id;
+            await function.deleteProduct(parameters);
+            return NoContent();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<List<productModel>>> getProductById(int id)
+        {
+            var function = new ProductData();
+            var parameters = new productModel();
+            parameters.id = id;
+            var list = await function.listProductById(parameters);
+            return list;
+        }
+    }
+}
---
+++ Data/ProductData.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineShop.Connection;
+using OnlineShop.Models;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace OnlineShop.Data
+{
+    public class ProductData
+    {
+        ConnectionDB cn = new ConnectionDB();
+
+        public async Task<List<productModel>> listProducts()
+        {
+            var list = new List<productModel>();
+            using (var sql = new SqlConnection(cn.ConnectionString()))
+            {
+                using (var cmd = new SqlCommand("listProducts", sql))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    await sql.OpenAsync();
+                    using (var reader = await cmd.ExecuteReaderAsync())
+                    {
+                        while(await reader.ReadAsync())
+                        {
+                            var product = new productModel();
+                            product.name = (string)reader["name"];
+                            product.description = (string)reader["description"];
+                            product.price = (decimal)reader["price"];
+                            list.Add(product);
+                        }
+                        return list;
+                    }
+                }
+            }
+        }
+
+        public async Task insertProduct(productModel parameters)
+        {
+            using (var sql = new SqlConnection(cn.ConnectionString()))
+            {
+                using (var cmd = new SqlCommand("insertProduct", sql))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("name", parameters.name);
+                    cmd.Parameters.AddWithValue("description", parameters.description);
+                    cmd.Parameters.AddWithValue("price", parameters.price);
+                    await sql.OpenAsync();
+                    await cmd.ExecuteNonQueryAsync();
+                }
+            }
+        }
+
+        public async Task updateProduct(productModel parameters)
+        {
+            using (var sql = new SqlConnection(cn.ConnectionString()))
+            {
+                using (var cmd = new SqlCommand("updateProduct", sql)) 
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("id", parameters.id);
+                    cmd.Parameters.AddWithValue("name", parameters.name);
+                    cmd.Parameters.AddWithValue("description", parameters.description);
+                    cmd.Parameters.AddWithValue("price", parameters.price);
+                    await sql.OpenAsync();
+                    await cmd.ExecuteNonQueryAsync();
+                }
+            }
+        }
+
+        public async Task deleteProduct(productModel parameters)
+        {
+            using(var sql = new SqlConnection(cn.ConnectionString()))
+            {
+                using (var cmd = new SqlCommand("deleteProduct", sql))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("id", parameters.id);
+                    await sql.OpenAsync();
+                    await cmd.ExecuteNonQueryAsync();
+                }
+            }
+        }
+
+        public async Task<List<productModel>> listProductById(productModel parameters)
+        {
+            var list = new List<productModel>();
+            using (var sql = new SqlConnection(cn.ConnectionString()))
+            {
+                using (var cmd = new SqlCommand("listProductById", sql))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("id", parameters.id);
+                    await sql.OpenAsync();
+                    using (var reader = await cmd.ExecuteReaderAsync())
+                    {
+                        while (await reader.ReadAsync())
+                        {
+                            var product = new productModel();
+                            product.name = (string)reader["name"];
+                            product.description = (string)reader["description"];
+                            product.price = (decimal)reader["price"];
+                            list.Add(product);
+                        }
+                        return list;
+                    }
+                }
+            }
+        }
+    }
+}
